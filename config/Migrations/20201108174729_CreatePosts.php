c0bda8f7efc0e874d74b650d053a88cc31293bea
Fernando Alves <fx2.sistemas@gmail.com>
2020-11-08 15:04:21 -0300

    dev-nando posts migrations

---
+++ config/Migrations/20201108174729_CreatePosts.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreatePosts extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('posts');
+        $table->addColumn('user_id', 'integer');
+
+        $table->addColumn('title', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('slug', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('body', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('password', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addIndex([
+            'slug',
+        
+            ], [
+            'name' => 'UNIQUE_SLUG',
+            'unique' => true,
+        ]);
+
+        $table->addForeignKey('user_id', 'users', ['id'],
+                            ['constraint'=>'user_id']);
+              
+              
+        $table->create();
+    }
+}
